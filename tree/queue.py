abc881201f36771820b2731cf0ccc82ee445048d
viveksharma697 <viveksharma697@gmail.com>
2022-04-02 22:30:06 +0530

    queue problems to be fixed

--- tree/queue.py
+++ tree/queue.py
@@ -1,3 +1,8 @@
+# Design a food ordering system where your python program will run two threads,
+
+# Place Order: This thread will be placing an order and inserting that into a queue. This thread places new order every 0.5 second. (hint: use time.sleep(0.5) function)
+# Serve Order: This thread will server the order. All you need to do is pop the order out of the queue and print it. This thread serves an order every 2 seconds. Also start this thread 1 second after place order thread is started.
+
 import threading
 import time
 
@@ -30,6 +35,7 @@ def place_orders(orders):
         print("Placing order for:",order)
         food_order_queue.enqueue(order)
         time.sleep(0.5)
+        
 
 
 def serve_orders():
@@ -38,7 +44,8 @@ def serve_orders():
         order = food_order_queue.dequeue()
         print("Now serving: ",order)
         time.sleep(2)
-
+        
+# some corrections to be done here
 if __name__ == '__main__':
     orders = ['pizza','samosa','pasta','biryani','burger']
     t1 = threading.Thread(target=place_orders, args=(orders,))
