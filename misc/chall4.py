d3aebdecf9e80d2f71338c91f16af1637ad74780
viveksharma697 <viveksharma697@gmail.com>
2022-04-11 21:55:56 +0530

    another problem

--- misc/chall4.py
+++ misc/chall4.py
@@ -1,23 +1,28 @@
+# write a function that accepts a number as a parameter. The function should return a number that is difference between the largest and smallest numbers that the digits can form in the number. For example, if the parameter is "213", the function should return "198", which is the result of 123 subtracted from 321.
+
+
+x = []
 
 
 def toString(List):
-    return ''.join(List)
- 
+    return x == x.append(''.join(List))
+
 
 def permute(a, l, r):
-
-    if l==r:        
-        print(toString(a))
+    if l == r:
+        return toString(a)
     else:
-    
-        for i in range(l,r+1):
+        for i in range(l, r+1):
             a[l], a[i] = a[i], a[l]
             permute(a, l+1, r)
             a[l], a[i] = a[i], a[l]
- 
-# need some repair work
-string = '123'
+
+
+string = "123"
 n = len(string)
 a = list(string)
 permute(a, 0, n-1)
- 
+y = [int(i) for i in x]
+y.sort()
+z = y[-1] - y[0]
+print(z)
