eda28abdfe63eee222881a84638ea8bcb0b6c8b5
viveksharma697 <viveksharma697@gmail.com>
2022-05-10 22:56:52 +0530

    some exercises on stack

--- misc/stack1.py
+++ misc/stack1.py
@@ -1,36 +1,23 @@
 # find the largest rectangle in histogram
-class Solution:
-    def largestRectangleArea(self, heights: List[int]) -> int:
-        n = len(heights)
-        left = [0] * n
-        right = [0] * n
-        stack = []
-        #getting the left elements
-        for i in range(n):
-            if not stack:
-                left[i] = 0
-            else:
-                while stack and heights[stack[-1]] >= heights[i]:
-                    stack.pop()
-                left[i] = stack[-1]+1 if stack else 0
-            stack.append(i)
-        stack.clear()
-        #getting the right elements
-        for i in range(n-1,-1,-1):
-            if not stack:
-                right[i] = n-1
-            else:
-                while stack and heights[stack[-1]] >= heights[i]:
-                    stack.pop()
-                right[i] = stack[-1]-1 if stack else n-1
-            stack.append(i)
 
-        #calculating the area
-        max_area = 0
-        for i in range(n):
-            height = heights[i]
-            width = right[i]-left[i]+1
-            area = height * width
-            if area > max_area:
-                max_area = area
-        return max_area
+def largestRectangleArea(self, heights):
+    stack = [-1]
+    max_area = 0
+    for i in range(len(heights)):
+        while stack[-1] != -1 and heights[stack[-1]] >= heights[i]:
+            current_height = heights[stack.pop()]
+            current_width = i - stack[-1] - 1
+            max_area = max(max_area, current_height * current_width)
+        stack.append(i)
+ 
+    while stack[-1] != -1:
+        current_height = heights[stack.pop()]
+        current_width = len(heights) - stack[-1] - 1
+        max_area = max(max_area, current_height * current_width)
+    return max_area
+
+a = [2,1,5,7,3,2]
+
+print(largestRectangleArea("check", a))
+
+    
